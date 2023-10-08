ae12b425598af719acd81fb24e9a733b1279c4f0
k1nho <kinhong99@gmail.com>
2023-10-08 18:58:31 -0400

    supress change message on lazy

--- lua/kin/lazy.lua
+++ lua/kin/lazy.lua
@@ -16,6 +16,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     {import = "kin.plugins"}
 }, {
+    change_detection = {notify = false,},
     checker = {
         enabled = true,
         notify = false,
