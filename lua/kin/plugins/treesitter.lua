b2e4ae12e58fbe74189b43ad46d3f21e1ec25f35
k1nho <kinhong99@gmail.com>
2023-10-08 20:47:33 -0400

    treesitter

---
+++ lua/kin/plugins/treesitter.lua
@@ -0,0 +1,60 @@
+-- TREESITTER
+-- syntax highlighting and some other neat features for working in vim
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "windwp/nvim-ts-autotag",
+    },
+    config = function()
+      local treesitter = require("nvim-treesitter.configs")
+
+      treesitter.setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+        autotag = { enable = true },
+        ensure_installed = {
+          "json",
+          "python",
+          "cpp",
+          "javascript",
+          "typescript",
+          "tsx",
+          "yaml",
+          "html",
+          "css",
+          "prisma",
+          "markdown",
+          "markdown_inline",
+          "vue",
+          "go",
+          "rust",
+          "graphql",
+          "bash",
+          "lua",
+          "vim",
+          "dockerfile",
+          "gitignore",
+          "query",
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
+        },
+        -- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
+        context_commentstring = {
+          enable = true,
+          enable_autocmd = false,
+        },
+      })
+    end,
+  },
+}
